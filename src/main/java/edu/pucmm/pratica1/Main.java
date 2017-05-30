15d036f49d6984294a97f7d85aa7d2462c85d5a5
Jhon <jhonakabane@gmail.com>
2017-05-30 13:51:52 -0400

    Se modifico el la forma de relaizar el conteo de lineas y se arreglo la funcion f para la correcta ejecucion del metodo post al servidor y la impresion de la respuesta del servidor

--- src/main/java/edu/pucmm/pratica1/Main.java
+++ src/main/java/edu/pucmm/pratica1/Main.java
@@ -8,6 +8,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -15,7 +16,11 @@ import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 
 import static spark.Spark.post;
@@ -27,15 +32,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Document doc = null;
-        boolean urlValida= false;
+        boolean urlValida = false;
         String url = "";
+        int totalLineas=0;
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
 
-        post("/practica1", (req, res) -> {
-            String texto = "Asignatura: "+req.queryParams("asignatura");
-            System.out.println(texto);
-            return texto;
-        });
+//        0)Pruebas con el servidor local
+//        post("/practica1", (req, res) -> {
+//            String texto = "Asignatura: "+req.queryParams("asignatura");
+//            System.out.println(texto);
+//            return texto;
+//        });
 
         do {
 
@@ -48,11 +55,13 @@ public class Main {
 
             try {
                 doc = Jsoup.connect(url).get();
-                urlValida=true;
-            } catch (Exception e) {
-            }
-        }while(!urlValida);
+                totalLineas=extractContent(url).split("\n").length;
 
+                urlValida = true;
+            } catch (Exception e) {
+                System.out.println("URL MALO XD");
+            }
+        } while (!urlValida);
 
 
 //        String html = "<html><head><title>First parse</title></head>"
@@ -60,28 +69,28 @@ public class Main {
 //        Document doc = Jsoup.parse(html);
 
 //        A)  Indicar la cantidad de lineas del recurso retornado.
-        System.out.println("Cantidad de lineas: "+doc.html().split("\n").length);
+        System.out.println("Cantidad de lineas: " + totalLineas);
 
 //        B)Indicar la cantidad de párrafos (p) que contiene el documento HTML .
-        System.out.println("Cantidad de parrafos: "+doc.select("p").size());
+        System.out.println("Cantidad de parrafos: " + doc.select("p").size());
 
 //        C) Indicar la cantidad de imágenes (img) dentro de los párrafos que
 //        contiene el archivo HTML.
         System.out.println("Cantidad de imagenes dentro de parrafos: "
-                +doc.select("p img").size());
+                + doc.select("p img").size());
 
 //        D) Indicar la cantidad de formularios (form) que contiene el HTML por
 //        categorizando por el método implementado POST o GET.
-        System.out.println("Cantidad de formularios (POST): "+doc.select("form[method=post]").size());
-        System.out.println("Cantidad de formularios (GET): "+doc.select("form[method=get]").size());
+        System.out.println("Cantidad de formularios (POST): " + doc.select("form[method=post]").size());
+        System.out.println("Cantidad de formularios (GET): " + doc.select("form[method=get]").size());
 
 //        E) Para cada formulario mostrar los campos del tipo input y su
 //        respectivo tipo que contiene en el documento HTML.
-        int contador=0;
+        int contador = 0;
         Elements forms = doc.select("form");
-        for (Element form: forms) {
+        for (Element form : forms) {
             contador++;
-            System.out.println("Form #"+contador);
+            System.out.println("Form #" + contador);
             System.out.println(forms.select("input"));
         }
 
@@ -90,17 +99,35 @@ public class Main {
 //        petición al servidor, con el parámetro llamado asignatura y valor
 //        practica1 y mostrar la respuesta por la salida estandar
 
+        System.out.println("Entro a los forms");
         forms = doc.select("form[method=post]");
-        for (Element form: forms) {
+        System.out.println("cantidad de forms:" + forms.size());
+        for (Element form : forms) {
             CloseableHttpClient httpclient = HttpClients.createDefault();
-
-            HttpPost httpPost = new HttpPost("http://localhost:4567/practica1");
+            String urlPost = url.split("/")[0] + "//" + url.split("/")[2] + form.attr("action");
+            System.out.println(urlPost);
+            HttpPost httpPost = new HttpPost(urlPost);
             List<NameValuePair> nvps = new ArrayList<NameValuePair>();
             nvps.add(new BasicNameValuePair("asignatura", "practica1"));
             httpPost.setEntity(new UrlEncodedFormEntity(nvps));
             CloseableHttpResponse response = httpclient.execute(httpPost);
+            System.out.println("respuesta:");
+            System.out.println(EntityUtils.toString(response.getEntity()));
         }
+    }
 
-
+    private static String extractContent (String urlString)
+            throws MalformedURLException, IOException {
+        URL url = new URL(urlString);
+        URLConnection urlConnection = url.openConnection();
+        InputStream is = urlConnection.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String content = "";
+        String linea = br.readLine();
+        while (null != linea) {
+            content += linea+"\n";
+            linea = br.readLine();
+        }
+        return content.trim();
     }
 }
